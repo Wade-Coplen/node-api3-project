4d0f24fed0b3d931ce9b7464599ae4dd00c8336e
Wade-Coplen <wadecoplen@gmail.com>
2020-03-19 20:19:36 -0700

    I- server.js and index.js added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const helmet = require('helmet')
 const server = express();
+
+const PostsRouter = require('./posts/postRouter');
+//built-in
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
