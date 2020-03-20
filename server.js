296137c65ad69e003bf21b17bb86122df497f0f9
Wade-Coplen <wadecoplen@gmail.com>
2020-03-19 20:22:27 -0700

    II- additional save

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const helmet = require('helmet')
 const server = express();
 
-const PostsRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter');
 //built-in
 server.use(express.json());
 
+//3rd party
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.use('./api/posts', postsRouter);
+server.use('./api/users', userRouter)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
@@ -15,3 +22,12 @@ server.get('/', (req, res) => {
 function logger(req, res, next) {}
 
 module.exports = server;
+
+
+//validateUserId()
+
+//validateUser()
+
+//validatePost()
+
+//
