bdc7b8e7bf5f8ce685a4f7f8e4178305b079df3b
Wade-Coplen <wadecoplen@gmail.com>
2020-03-21 05:28:54 -0700

    starting userRouter

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ server.use(express.json());
 
 //3rd party
 server.use(helmet());
-server.use(morgan('dev'));
-
+// server.use(morgan('dev'));
+//----custom-----
+server.use(loggerMethod);
+server.use(loggerUrl);
+server.use(loggerTimeStamp);
+server.use(validateUserId);
+server.use(validateUser);
+server.use(validatePost);
+//================================
 server.use('./api/posts', postsRouter);
 server.use('./api/users', userRouter)
 server.get('/', (req, res) => {
@@ -19,15 +26,48 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
-
-module.exports = server;
-
-
+function loggerMethod(req, res, next) {
+  console.log(`${req.method} Request`);
+  next();
+}
+function loggerUrl(req, res, next) {
+  console.log(`${req.url} Request`);
+  next();
+}
+function loggerTimeStamp(req, res, next) {
+  console.log(`${req.timestamp} Request`);
+  next();
+}
 //validateUserId()
-
+function validateUserId(req, res, next) {
+  const userId = req.id;
+  if(userId) {
+    req.user = userId;
+  } else {
+    !userId;
+    res.status(400).json({messge: 'invalid user id'})
+  }
+  next();
+}
 //validateUser()
-
+function validateUser(req, res, next) {
+  const body = req.body;
+  const name = req.body.name;
+  if(!body) {
+    res.status(400).json({message: 'missing user'})
+  } else {
+    body !== name;
+      res.status(400).json({message: 'missing required name field'})
+  }
+  next();
+}
 //validatePost()
-
+function validatePost(req, res, next) {
+  const body = req.body;
+  const text = req.body.text;
+  if(!body) {
+    res.status(400).json({message: 'missing post data'});
+  }
+}
 //
+module.exports = server;
